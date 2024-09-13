7e81fc40ddcca65d2f0328c28424930ce2638bb0
hasib1010 <asifmahmud664@gmail.com>
2024-09-13 15:24:34 +0600

    Update dependencies and fix vulnerabilities

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        'react-quill/dist/quill.snow.css', 
-      ],
-    },
-  },
 });
